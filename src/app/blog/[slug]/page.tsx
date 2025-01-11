313718b2ccd91f2f25864112de1c11a8d3874000
Kaveesh Khattar <kaveeshkhattar@gmail.com>
2025-01-12 01:04:06 +0530

    Added types to params

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
 export async function generateStaticParams() {
   const postsDirectory = path.join(process.cwd(), '/public/content');
   const filenames = fs.readdirSync(postsDirectory);
@@ -11,8 +17,8 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPost({ params }) {
-  const { slug } = await params;
+export default async function BlogPost({ params }: Params) {
+  const { slug } = params;
   const filePath = path.join(process.cwd(), '/public/content', `${slug}.mdx`);
   const content = fs.readFileSync(filePath, 'utf8');
 
