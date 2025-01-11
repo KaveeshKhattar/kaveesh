15f949565b5087c7464596d04bfe36d37908cd3e
Kaveesh Khattar <kaveeshkhattar@gmail.com>
2025-01-12 01:15:16 +0530

    Updated semantics

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,26 +1,24 @@
-import fs from 'fs';
-import path from 'path';
-import { MDXRemote } from 'next-mdx-remote/rsc';
-
-type Params = {
-  params: {
-    slug: string;
-  };
-};
+import fs from "fs";
+import path from "path";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 export async function generateStaticParams() {
-  const postsDirectory = path.join(process.cwd(), '/public/content');
+  const postsDirectory = path.join(process.cwd(), "/public/content");
   const filenames = fs.readdirSync(postsDirectory);
-  
+
   return filenames.map((filename) => ({
-    slug: filename.replace(/\.mdx$/, ''),
+    slug: filename.replace(/\.mdx$/, ""),
   }));
 }
 
-export default async function BlogPost({ params }: Params) {
-  const { slug } = params;
-  const filePath = path.join(process.cwd(), '/public/content', `${slug}.mdx`);
-  const content = fs.readFileSync(filePath, 'utf8');
+export default async function BlogPost({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const filePath = path.join(process.cwd(), "/public/content", `${slug}.mdx`);
+  const content = fs.readFileSync(filePath, "utf8");
 
   return (
     <article>
